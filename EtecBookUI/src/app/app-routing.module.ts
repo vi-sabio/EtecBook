ca3fa1c6633f23bef8dee452cde61343cf27ad4c
Vinicius Sabio <vinicios_sabio@hotmail.com>
2023-05-18 20:32:26 -0300

    color-models
    fazendo botao do tema claro/escuro

---
+++ EtecBookUI/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule, } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
